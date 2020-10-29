1ae71681ac426c1763cbbd0c6ddef1c9e77ad35f
StarEpicBoy <55774240+Lakhankumawat@users.noreply.github.com>
2020-10-29 09:51:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Superhero from "./SuperHero";
+import Header from "./Header";
+import Footer from "./Footer";
 export default function App() {
+  const handleRequest = (name) => {
+    const Query = name;
+  };
+
   return (
     <div className="App">
+      <Header onRequest={handleRequest} />
       <Superhero />
+      <Footer />
     </div>
   );
 }
